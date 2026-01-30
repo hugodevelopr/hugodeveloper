3a3b9060de7ef24d3ccc4fa1dfc26b931f39c21f
Hugo Moura <hugo.rodrigues.moura@gmail.com>
2026-01-30 19:14:48 -0300

    fix: removed duplicate file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       defaultMode: 'dark',
-      disableSwitch: false,
+      disableSwitch: true,
       respectPrefersColorScheme: true,
     },
     navbar: {
